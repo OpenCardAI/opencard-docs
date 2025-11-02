134ecde076ca44b0861e4766a14bb594137697d8
brysoncamp <bryson@bryson.camp>
2025-11-02 22:56:38 +1000

    theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { CreditCard } from 'lucide-react'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+      <CreditCard style={{ height: '1.25rem', width: '1.25rem' }} />
+      <span style={{ fontWeight: 600, fontSize: '1.125rem' }}>OpenCard</span>
+    </div>
+  ),
+  logoLink: 'https://opencard.ai',
+  primaryHue: 0,
+  primarySaturation: 0,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/OpenCardAI/OpenCard',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/OpenCardAI/OpenCard',
   footer: {
     text: 'Nextra Docs Template',
   },
